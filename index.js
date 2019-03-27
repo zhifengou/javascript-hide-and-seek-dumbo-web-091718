2d8c965ae4520d230a0ad60d4989bb76d6d46f6b
zhifeng ou <zhifengou@hotmail.com>
2019-03-27 18:14:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.getElementById("nested").querySelector(".target");
+}
+
+function deepestChild(){
+  let node = document.querySelector('div#grand-node');
+  let children = node.children[0]
+  while (children){
+    node=node.children[0]
+    children=node.children[0]
+  }
+  return node
+}
+
+
+function increaseRankBy(n){
+  let list = document.querySelectorAll("ul.ranked-list");
+  for(let i=0;i<list.length;i++){
+    let children = list[i].children;
+    for (let j=0;j<children.length;j++){
+      children[j].innerHTML = parseInt(children[j].innerHTML)+n;
+    }
+  }
+} 
 
